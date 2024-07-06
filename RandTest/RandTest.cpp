c77ec445c030346bb3f01ae02dc5a6691bdf52b0
Jason McKinney <meter_1@yahoo.com>
2024-07-05 19:45:46 -0500

    Now added

--- RandTest/RandTest.cpp
+++ RandTest/RandTest.cpp
@@ -4,20 +4,21 @@
 #include <iostream>
 #include "xirand.h"
 #include "rng/mersennetwister.h"
+#include <chrono>
 
 int main()
 {
-	long sta = 1720222200;
-	long end = 1720328399;
-long i = 0;
+	auto sta = std::chrono::system_clock::now();
+	auto end = sta + std::chrono::seconds(3600);
+	std::chrono::system_clock::time_point i = sta;
 
-	for (i = sta; i < end; i += 1) {
+	for (i = sta; i < end; i += std::chrono::seconds(1)) {
 		float rand = xirand::GetRandomNumber(1.);
 
 		if (rand <.05)
 		{
 			// Convert timestamp to human-readable time
-			std::time_t timestamp = i; // Divide by 1000 to convert to seconds
+			std::time_t timestamp = std::chrono::system_clock::to_time_t(i);
 			char buffer[26];
 			ctime_s(buffer, sizeof(buffer), &timestamp);
 
